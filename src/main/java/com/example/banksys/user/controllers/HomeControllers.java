3be8b2aa2ffd1bd2c7c66664a59b699fcd2d2d8c
Elie nsampi <elientumba@live.fr>
2018-05-17 14:42:07 +0800

    home controller's route implemented

--- src/main/java/com/example/banksys/user/controllers/HomeControllers.java
+++ src/main/java/com/example/banksys/user/controllers/HomeControllers.java
@@ -1,8 +1,27 @@
 package com.example.banksys.user.controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
+/**
+ * home controller class
+ */
 
 @Controller
 public class HomeControllers {
+
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+
+
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+
 }
