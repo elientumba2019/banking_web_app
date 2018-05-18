2010c975a4aa82dfebfc38b015d157ab2855b415
Elie nsampi <elientumba@live.fr>
2018-05-18 14:00:47 +0800

    adding few security classes to the model

---
+++ src/main/java/com/example/banksys/user/domain/security/Role.java
@@ -0,0 +1,49 @@
+package com.example.banksys.user.domain.security;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+public class Role {
+
+    @Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long roleId;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role"  ,
+            cascade = CascadeType.ALL ,
+            fetch = FetchType.LAZY )
+    private Set<UserRole> userRoles = new HashSet<>();
+
+
+    public Role() {
+    }
+
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+}
